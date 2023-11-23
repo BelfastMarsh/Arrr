582b51a1e06e1c50fc8f13c4a5b82cccb14d34c9
Peter Marsh <Peter.Marsh@cso.ie>
2023-11-23 09:37:06 +0000

    3

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("a"),
+    titlePanel("Ar"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
